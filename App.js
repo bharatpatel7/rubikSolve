10db9cb34cb01a923e216e7e079e9c6e9cd6b3ba
Bharat Garsondiya <bharat.garsondiya2609@gmail.com>
2025-05-19 14:10:06 -0400

    DevOps

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Rubik’s Cube Solver - Coming Soon!</Text>
+    </View>
+  );
+}
